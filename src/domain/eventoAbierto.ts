04178491942d36d992a7f79ae909f9552ebca466
Mauro Casciati <maurocasciati@cnea.gov.ar>
2018-10-25 16:45:39 -0300

    Correccion error en constructor de eventoCerrado y eventoAbierto

--- src/domain/eventoAbierto.ts
+++ src/domain/eventoAbierto.ts
@@ -7,21 +7,9 @@ export class EventoAbierto extends Evento{
     constructor(nombre:string,fechaMaximaConfirmacion:Date,fechaDesde:Date,fechaHasta:Date,locacion:Locacion){
         super()
         this.nombre = nombre
-        this.fechaMaximaConfirmacion = moment({
-            'year': fechaMaximaConfirmacion.getFullYear(), 
-            'month': fechaMaximaConfirmacion.getMonth(),
-            'day': fechaMaximaConfirmacion.getDay()
-        });
-        this.fechaDesde = moment({
-            'year': fechaDesde.getFullYear(), 
-            'month': fechaDesde.getMonth(),
-            'day': fechaDesde.getDay()
-        });
-        this.fechaHasta = moment({
-            'year': fechaHasta.getFullYear(), 
-            'month': fechaHasta.getMonth(),
-            'day': fechaHasta.getDay()
-        });
+        this.fechaMaximaConfirmacion = moment(fechaMaximaConfirmacion)
+        this.fechaDesde = moment(fechaDesde)
+        this.fechaHasta = moment(fechaHasta)
         this.locacion = locacion
     }
 }
--- src/domain/eventoCerrado.ts
+++ src/domain/eventoCerrado.ts
@@ -6,21 +6,9 @@ export class EventoCerrado extends Evento {
     constructor(nombre:string,fechaMaximaConfirmacion:Date,fechaDesde:Date,fechaHasta:Date,locacion:Locacion){
         super()
         this.nombre = nombre
-        this.fechaMaximaConfirmacion = moment({
-            'year': fechaMaximaConfirmacion.getFullYear(), 
-            'month': fechaMaximaConfirmacion.getMonth(),
-            'day': fechaMaximaConfirmacion.getDay()
-        });
-        this.fechaDesde = moment({
-            'year': fechaDesde.getFullYear(), 
-            'month': fechaDesde.getMonth(),
-            'day': fechaDesde.getDay()
-        });
-        this.fechaHasta = moment({
-            'year': fechaHasta.getFullYear(), 
-            'month': fechaHasta.getMonth(),
-            'day': fechaHasta.getDay()
-        });
+        this.fechaMaximaConfirmacion = moment(fechaMaximaConfirmacion)
+        this.fechaDesde = moment(fechaDesde)
+        this.fechaHasta = moment(fechaHasta)
         this.locacion = locacion
     }
 }
